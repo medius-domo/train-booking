c082e95cee145c6ed2a48f3640086bead48a4fdd
MEDIUS <mediusdomo@gmail.com>
2025-01-19 15:18:42 +0300

    inserted harmberg menu

--- mobilenav.js
+++ mobilenav.js
@@ -1,16 +1,12 @@
+
 $(document).ready(function () {
-    // Open the side menu
-    $('#menu-btn').click(function () {
-      if ($('#sidemenu').css('right') === '-100%') {
-        $('#sidemenu').css('right', '0');
-      }
-    });
-  
-    // Close the side menu
-    $('#close-btn').click(function () {
-      if ($('#sidemenu').css('right') === '0px') {
-        $('#sidemenu').css('right', '-100%');
-      }
-    });
+  // Open the side menu
+  $('#menu-btn').click(function () {
+    $('#sidemenu').addClass('open');
   });
-  
+
+  // Close the side menu
+  $('#close-btn').click(function () {
+    $('#sidemenu').removeClass('open');
+  });
+});
