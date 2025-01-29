7b8106744e49104f7de9ddf83ece4a67817c2737
MEDIUS <mediusdomo@gmail.com>
2025-01-29 22:58:50 +0300

    learnt ajax & database operation

--- trainzilizopo.js
+++ trainzilizopo.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 const trainData = [
     { id: 1, train: "L172 - DELUXE", route: "DAR ES SALAAM - ARUSHA", departure: "31/01/2025 02:30 PM, JUMAA", arrival: "01/02/2025 12:10 PM, JUMAMOSI" },
     { id: 2, train: "YD161 - DELUXE", route: "TABORA - DAR ES SALAAM", departure: "30/01/2025 05:50 PM, ALHAMISI", arrival: "31/01/2025 01:36 PM, JUMAA" },
@@ -5,7 +6,27 @@ const trainData = [
 ];
 
 const tbody = document.querySelector("table tbody");
-
+    $.ajax({
+        url: "http://127.0.0.1/backendtest/fetch_trains.php", // PHP script to fetch data
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            let rows = "";
+            response.forEach(function (train) {
+                rows += `<tr>
+                    <td>${train.train_id}</td>
+                    <td>${train.train_name}</td>
+                    <td>${train.train_route}</td>
+                    <td>${train.departure_time}</td>
+                    <td>${train.arrival_time}</td>
+                </tr>`;
+            });
+            $("#trains").html(rows);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error fetching train data:", error);
+        }
+    });
 trainData.forEach((train) => {
     const row = document.createElement("tr");
     row.innerHTML = `
@@ -15,5 +36,6 @@ trainData.forEach((train) => {
         <td>${train.departure}</td>
         <td>${train.arrival}</td>
     `;
-    tbody.appendChild(row);
+    //tbody.appendChild(row);
 });
+})
